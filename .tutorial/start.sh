a5a6b9312552b9918eaea9be47effa542bf32b4b
Louis Lambeau <llambeau@quadrabee.com>
2020-12-12 12:47:17 +0100

    Bring webspicy

--- .tutorial/start.sh
+++ .tutorial/start.sh
@@ -3,7 +3,8 @@
 clear
 echo "Please wait a little bit, the tutorial is loading..."
 gp await-port 3000
+clear
 cat .tutorial/banner.txt
-gp open src/index.js
+gp open formaldoc/version/get.yml
 
-ttyecho /dev/pts/1 "curl http://localhost:3000/version"&
+ttyecho /dev/pts/1 "webspicy formaldoc/config.rb"&
