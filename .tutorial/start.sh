7666bdbd547e48f1928a184d98f5705b9a901762
Louis Lambeau <llambeau@quadrabee.com>
2020-12-12 13:24:52 +0100

    NodeJS API

--- .tutorial/start.sh
+++ .tutorial/start.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
 clear
+echo "Please wait a little bit, the tutorial is loading..."
+gp await-port 3000
 cat .tutorial/banner.txt
+gp open src/index.js
+
+ttyecho /dev/pts/1 "curl http://localhost:3000/version"&
